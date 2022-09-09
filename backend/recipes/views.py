6291b40ed056838ded9125b6fe49d589a80ba08d
Nikita Natalenko <nnikitanatalenko@gmail.com>
2022-09-09 12:52:04 +0700

    Finish PATCH method for recipe object endpoint

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -3,8 +3,6 @@
 from rest_framework.filters import SearchFilter
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from .models import Ingredient, Recipe, RecipeIngredientAmount, Tag
@@ -36,51 +34,26 @@ class RecipeViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     queryset = Recipe.objects.all()
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+    def perform_create(self, serializer):
         ingredients = serializer.validated_data.pop('recipeingredientamount_set')
-        created_recipe = serializer.save(author=request.user)
+        created_recipe = serializer.save(author=self.request.user)
+        self.bulk_create(created_recipe, ingredients)
 
+    def perform_update(self, serializer):
+        if serializer.validated_data.get('recipeingredientamount_set', False):
+            ingredients = serializer.validated_data.pop('recipeingredientamount_set')
+            updated_recipe = serializer.save()
+            updated_recipe.ingredients.clear()
+            self.bulk_create(updated_recipe, ingredients)
+        else:
+            serializer.save()
+
+    def bulk_create(self, recipe, ingredients):
         ingredients_for_recipe = [
             RecipeIngredientAmount(
-                recipe=created_recipe,
+                recipe=recipe,
                 ingredient_id=ingredient['ingredient']['id'],
                 amount=ingredient['amount']
             ) for ingredient in ingredients
         ]
-        created_recipe.recipeingredientamount_set.bulk_create(ingredients_for_recipe)
-
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=HTTP_201_CREATED, headers=headers)
-
-    # def update(self, request, *args, **kwargs):
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance, data=request.data, partial=True)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-
-    #     ingredients = serializer.validated_data.get('recipeingredientamount_set',
-    #                                                 False)
-    #     if ingredients:
-    #         ingredients = serializer.validated_data.pop('recipeingredientamount_set')
-    #         print(ingredients)
-    #         for ingredient in ingredients:
-
-    #         ingredients_for_recipe = [
-    #             RecipeIngredientAmount(
-    #                 recipe=instance,
-    #                 ingredient_id=ingredient['ingredient']['id'],
-    #                 amount=ingredient['amount']
-    #             ) for ingredient in ingredients
-    #         ]
-    #         instance.recipeingredientamount_set.bulk_create(ingredients_for_recipe)
-
-    #     serializer = RecipeSerializer(
-    #         serializer.instance,
-    #         context={'request': self.request},
-    #     )
-
-    #     if getattr(instance, '_prefetched_objects_cache', None):
-    #         instance._prefetched_objects_cache = {}
-    #     return Response(serializer.data)
+        recipe.recipeingredientamount_set.bulk_create(ingredients_for_recipe)
